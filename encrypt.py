977aa10489dc8160e5a4ef0fa3cc39f383798583
CrazyProger1 <crazyproger1@gmail.com>
2022-06-21 14:39:55 +0300

    Add file encrypting

---
+++ encrypt.py
@@ -0,0 +1,68 @@
+from typing import Iterable
+import subprocess
+from Crypto.Cipher import AES
+import os
+import struct
+from Crypto import Random
+
+
+def encrypt_file(key: bytes | str, in_filename: str, out_filename: str | None = None, chunk_size: int = 64 * 1024):
+    """ Encrypts a file using AES (CBC mode) with the
+        given key.
+
+        key:
+            The encryption key - a string that must be
+            either 16, 24 or 32 bytes long. Longer keys
+            are more secure.
+
+        in_filename:
+            Name of the input file
+
+        out_filename:
+            If None, '<in_filename>.enc' will be used.
+
+        chunksize:
+            Sets the size of the chunk which the function
+            uses to read and encrypt the file. Larger chunk
+            sizes can be faster for some files and machines.
+            chunksize must be divisible by 16.
+    """
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+
+    if type(key) is str:
+        key = key.encode("utf-8")
+
+    # iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    iv = Random.new().read(AES.block_size)
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    # 1234567890qwertyuiopasdfghjklzxc
+    file_size = os.path.getsize(in_filename)
+
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(struct.pack('<Q', file_size))
+            outfile.write(iv)
+
+            while True:
+                chunk = infile.read(chunk_size)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' '.encode() * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk))
+
+
+
+def encrypt_files(files: Iterable[str], password1: str, password2: str, out_filepath: str):
+    command = f'''7z a -t7z encrypted.7z "{'" "'.join(files)}" -p"{password1}"'''
+    code = subprocess.call(command)
+
+    print("7z exited with code: " + str(code))
+
+    encrypt_file(password2, "encrypted.7z", out_filepath)
+    print("Files encrypted. Cleaning up")
+
+    os.remove("encrypted.7z")
+    # print(code)
--- encrypt_dialog.py
+++ encrypt_dialog.py
@@ -1,6 +1,8 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from styles import *
 from config import *
+from encrypt import encrypt_files
+import threading
 
 
 class EncryptDialogUI(QtWidgets.QDialog):
@@ -17,37 +19,37 @@ class EncryptDialogUI(QtWidgets.QDialog):
         self.btn_select_out_file: None | QtWidgets.QPushButton = None
         self.btn_encrypt: None | QtWidgets.QPushButton = None
         self.btn_select_out_pwd_file: None | QtWidgets.QPushButton = None
+
+        self.output_filepath = ""
+        self.output_password_filepath = ""
         self.setup()
 
     def setup(self):
         self.setObjectName("encrypt_dialog")
         self.setFixedSize(481, 734)
         self.setStyleSheet(ENCRYPT_DIALOG_STYLE)
+
         self.btn_add_file = QtWidgets.QPushButton(self)
         self.btn_add_file.setGeometry(QtCore.QRect(10, 450, 131, 41))
         self.btn_add_file.setStyleSheet(TEXT_STYLE)
         self.btn_add_file.setObjectName("btn_add_file")
+        self.btn_add_file.clicked.connect(self.add_file)
 
         self.btn_delete_file = QtWidgets.QPushButton(self)
         self.btn_delete_file.setGeometry(QtCore.QRect(150, 450, 131, 41))
         self.btn_delete_file.setStyleSheet(TEXT_STYLE)
         self.btn_delete_file.setObjectName("btn_delete_file")
+        self.btn_delete_file.clicked.connect(self.delete_file)
 
         self.btn_clear_list = QtWidgets.QPushButton(self)
         self.btn_clear_list.setGeometry(QtCore.QRect(290, 450, 181, 41))
         self.btn_clear_list.setStyleSheet(TEXT_STYLE)
         self.btn_clear_list.setObjectName("btn_clear_list")
+        self.btn_clear_list.clicked.connect(self.clear_file_list)
 
         self.lst_file_list = QtWidgets.QListWidget(self)
         self.lst_file_list.setGeometry(QtCore.QRect(10, 10, 461, 421))
         self.lst_file_list.setObjectName("lst_file_list")
-
-        item = QtWidgets.QListWidgetItem()
-        self.lst_file_list.addItem(item)
-        item = QtWidgets.QListWidgetItem()
-        self.lst_file_list.addItem(item)
-        item = QtWidgets.QListWidgetItem()
-        self.lst_file_list.addItem(item)
 
         self.edit_password1 = QtWidgets.QLineEdit(self)
         self.edit_password1.setGeometry(QtCore.QRect(10, 530, 461, 21))
@@ -65,7 +67,7 @@ class EncryptDialogUI(QtWidgets.QDialog):
         self.edit_password2.setObjectName("edit_password2")
 
         self.txt_password2 = QtWidgets.QLabel(self)
-        self.txt_password2.setGeometry(QtCore.QRect(10, 560, 291, 16))
+        self.txt_password2.setGeometry(QtCore.QRect(10, 560, 410, 16))
         self.txt_password2.setStyleSheet(TEXT_STYLE)
         self.txt_password2.setObjectName("txt_password2")
 
@@ -73,19 +75,66 @@ class EncryptDialogUI(QtWidgets.QDialog):
         self.btn_select_out_file.setGeometry(QtCore.QRect(10, 610, 221, 41))
         self.btn_select_out_file.setStyleSheet(TEXT_STYLE)
         self.btn_select_out_file.setObjectName("btn_select_out_file")
+        self.btn_select_out_file.clicked.connect(self.select_output_file)
 
         self.btn_select_out_pwd_file = QtWidgets.QPushButton(self)
         self.btn_select_out_pwd_file.setGeometry(QtCore.QRect(250, 610, 221, 41))
         self.btn_select_out_pwd_file.setStyleSheet(TEXT_STYLE)
         self.btn_select_out_pwd_file.setObjectName("btn_select_out_pwd_file")
+        self.btn_select_out_pwd_file.clicked.connect(self.select_output_password_file)
 
         self.btn_encrypt = QtWidgets.QPushButton(self)
         self.btn_encrypt.setGeometry(QtCore.QRect(10, 670, 461, 51))
         self.btn_encrypt.setStyleSheet(TEXT_STYLE)
         self.btn_encrypt.setObjectName("btn_encrypt")
+        self.btn_encrypt.clicked.connect(self.encrypt)
 
         self.retranslate_ui()
         QtCore.QMetaObject.connectSlotsByName(self)
+
+    def add_file(self):
+        filepath = QtWidgets.QFileDialog.getOpenFileName(self, "Select a file", "/")[0]
+        item = QtWidgets.QListWidgetItem()
+        item.setText(filepath)
+        self.lst_file_list.addItem(item)
+
+    def delete_file(self):
+        selected_items = self.lst_file_list.selectedItems()
+        if len(selected_items) > 0:
+            selected_item = selected_items[0]
+            self.lst_file_list.takeItem(self.lst_file_list.indexFromItem(selected_item).row())
+
+    def clear_file_list(self):
+        self.lst_file_list.clear()
+
+    def select_output_file(self):
+        self.output_filepath = QtWidgets.QFileDialog.getSaveFileName(
+            self,
+            "Select an output file",
+            "/",
+            filter="*.sf"
+        )[0]
+
+    def select_output_password_file(self):
+        self.output_password_filepath = QtWidgets.QFileDialog.getSaveFileName(
+            self,
+            "Select an output password file",
+            "/"
+        )[0]
+
+    def encrypt(self):
+        password1 = self.edit_password1.text()
+        password2 = self.edit_password2.text()
+        files = []
+        for index in range(self.lst_file_list.count()):
+            files.append(self.lst_file_list.item(index).text())
+
+        threading.Thread(
+            target=encrypt_files,
+            args=[files, password1, password2, self.output_filepath],
+            kwargs={}
+        ).start()
+        # encrypt_files(files, password1, password2, self.output_filepath)
 
     def retranslate_ui(self):
         _translate = QtCore.QCoreApplication.translate
@@ -97,15 +146,10 @@ class EncryptDialogUI(QtWidgets.QDialog):
         self.setWindowTitle(_translate("encrypt_dialog", f"Safe V{VERSION} - encryption"))
         __sortingEnabled = self.lst_file_list.isSortingEnabled()
         self.lst_file_list.setSortingEnabled(False)
-        item = self.lst_file_list.item(0)
-        item.setText(_translate("encrypt_dialog", "D:/SomeFile.txt"))
-        item = self.lst_file_list.item(1)
-        item.setText(_translate("encrypt_dialog", "D:/Programming/Python/Projects/Safe/resources/logo.ico"))
-        item = self.lst_file_list.item(2)
-        item.setText(_translate("encrypt_dialog", "D:/SomeFile.exe"))
         self.lst_file_list.setSortingEnabled(__sortingEnabled)
         self.txt_password1.setText(_translate("encrypt_dialog", "Password 1 (remember it)"))
-        self.txt_password2.setText(_translate("encrypt_dialog", "Password 2 (will be saved to a file)"))
+        self.txt_password2.setText(
+            _translate("encrypt_dialog", "Password 2 (will be saved to a file, you can store it on USB)"))
         self.btn_select_out_file.setText(_translate("encrypt_dialog", "Select output file"))
         self.btn_select_out_pwd_file.setText(_translate("encrypt_dialog", "Select output password file"))
         self.btn_encrypt.setText(_translate("encrypt_dialog", "Encrypt"))
